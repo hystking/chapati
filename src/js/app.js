0a901a4775fabf00a4161ddc97c7a09d2066d04e
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2016-06-12 23:39:04 +0900

    add example routing structure

---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import routes from "./routes"
+
+routes(window.location.pathname)
---
+++ src/js/routes.js
@@ -0,0 +1,9 @@
+import index from "./index"
+
+export default function routes(path) {
+  switch(path) {
+    case "/":
+    case "/index.html":
+      return index()
+  }
+}
